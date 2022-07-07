4c88ca26412437477efaaf76d662d4c81b9f5144
PabloDavidAR <pablo.arandarod@gmail.com>
2022-07-08 01:47:11 +0200

    Initial commit

--- build/map_generation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/Documents/robotic_sim/2_wheeled/src/map_generation
-# Build directory: /home/pablo/Documents/robotic_sim/2_wheeled/build/map_generation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/toycar/CMakeFiles/toycar_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/toycar_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/toycar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
